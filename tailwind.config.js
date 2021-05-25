fd1cf3994eb0d2bf88ed4af5a5b477eca12832fe
Carlos Silva <csilva2810@gmail.com>
2021-05-25 11:32:55 -0300

    Initial configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkmode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
