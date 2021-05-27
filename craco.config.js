12f7fcdfa3ec896f93abba42d734805228e96e87
Carlos Silva <csilva2810@gmail.com>
2021-05-27 19:04:25 -0300

    Config bundle analyzer

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,24 @@
-module.exports = {
-  style: {
-    postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")],
+const { whenProd } = require("@craco/craco");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = () => {
+  const analyzerMode = process.env.REACT_APP_INTERACTIVE_ANALYZE
+    ? "server"
+    : "json";
+
+  return {
+    webpack: {
+      plugins: {
+        add: [
+          ...whenProd(() => [new BundleAnalyzerPlugin({ analyzerMode })], []),
+        ],
+      },
     },
-  },
+    style: {
+      postcss: {
+        plugins: [require("tailwindcss"), require("autoprefixer")],
+      },
+    },
+  };
 };
